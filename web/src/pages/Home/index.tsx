84b17c9edb17de10ce891f1d7c0603b5b36a672d
rauber <andrerauber2006@gmail.com>
2023-11-13 16:35:51 -0300

    modify style in Home and in Header

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import './style.css'
 import Header from "../../components/Header"
 
 const Home = () => {
@@ -7,6 +8,81 @@ const Home = () => {
   return (
     <>
       <Header search={search} />
+      <h1 className="center-text"> Compre Agora </h1>
+      <div className='grid center products'>
+        <div>
+          <img src="" alt="" />
+          <h1>Nome Produto</h1>
+          <div className='flex'>
+            <>*</>
+            <>*</>
+            <>*</>
+            <>*</>
+            <>*</>
+          </div>
+          <div className='flex space'>
+            <div className="flex center">
+              <h2> <span className='green'> R$300.00 </span></h2>
+              <h3> <span className='pink'> 30% off </span> </h3>
+            </div>
+            <button> Ver Mais </button>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h1>Nome Produto</h1>
+          <div className='flex'>
+            <>*</>
+            <>*</>
+            <>*</>
+            <>*</>
+            <>*</>
+          </div>
+          <div className='flex space'>
+            <div className="flex center">
+              <h2> <span className='green'> R$300.00 </span></h2>
+              <h3> <span className='pink'> 30% off </span> </h3>
+            </div>
+            <button> Ver Mais </button>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h1>Nome Produto</h1>
+          <div className='flex'>
+            <>*</>
+            <>*</>
+            <>*</>
+            <>*</>
+            <>*</>
+          </div>
+          <div className='flex space'>
+            <div className="flex center">
+              <h2> <span className='green'> R$300.00 </span></h2>
+              <h3> <span className='pink'> 30% off </span> </h3>
+            </div>
+            <button> Ver Mais </button>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h1>Nome Produto</h1>
+          <div className='flex'>
+            <>*</>
+            <>*</>
+            <>*</>
+            <>*</>
+            <>*</>
+          </div>
+          <div className='flex space'>
+            <div className="flex center">
+              <h2> <span className='green'> R$300.00 </span></h2>
+              <h3> <span className='pink'> 30% off </span> </h3>
+            </div>
+            <button> Ver Mais </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
